7e59ba7cd31199ec34f10dc50d16eb46c17662b0
Hamza Zaidi <humza.zaidi@gmail.com>
2021-03-27 12:04:05 -0400

    Sending meta data with headers

--- apps/marvel-wiki/src/app/components/nav-bar.tsx
+++ apps/marvel-wiki/src/app/components/nav-bar.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import { useHistory } from "react-router-dom";
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
@@ -8,38 +9,38 @@ import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import logo from '../../assets/logo.png';
+import { Autocomplete } from '@material-ui/lab';
+import { Avatar as AvatarMaterial, CircularProgress, TextField } from '@material-ui/core';
+import { useLazyQuery } from '@apollo/client';
+import { GET_CHARACTERS } from '../queries';
+import { Avatar } from '@marvel-wiki/api-interfaces';
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            flexGrow: 1,
+            flexGrow: 1
         },
         title: {
             display: 'block',
             width: 150,
             height: 30
         },
+        medium: {
+            width: theme.spacing(5),
+            height: theme.spacing(5),
+            marginRight: theme.spacing(2),
+        },
         search: {
             position: 'relative',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            '&:hover': {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
+            backgroundColor: 'inherit',
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(1),
+                width: 'auto',
             },
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
         },
         inputRoot: {
-            color: 'inherit',
+            color: 'white',
         },
         inputInput: {
             padding: theme.spacing(1, 1, 1, 0),
@@ -58,22 +59,65 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 const Navbar = () => {
     const classes = useStyles();
+    const [searchText, setSearchText] = useState('');
+    const history = useHistory();
+    const [loadCharacters, { called, loading, data }] = useLazyQuery(
+        GET_CHARACTERS,
+        { variables: { nameStartsWith: searchText } }
+    );
+    useEffect(() => {
+        if (searchText) {
+            loadCharacters();
+        }
+    }, [searchText]);
+
+    const handleChange = (searchText: string) => {
+        setSearchText(searchText);
+    }
+    const avatar = (thumbnail: Avatar): string => `${thumbnail.path}.${thumbnail.extension}`
     return (
         <div className={classes.root}>
             <AppBar position="sticky">
-                <Toolbar variant="dense">
+                <Toolbar>
                     <img className={classes.title} src={logo} />
                     <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
+                        <Autocomplete
+                            id="combo-box-demo"
+                            options={data?.characters || []}
+                            getOptionLabel={(option: any) => option && option.name}
+                            style={{ width: 300 }}
+                            renderOption={(option) => (
+                                <React.Fragment>
+                                    <span>
+                                        <AvatarMaterial src={avatar(option.thumbnail)} className={classes.medium} />
+                                    </span>
+                                    { option.name}
+                                </React.Fragment>
+                            )}
+                            renderInput={params => (
+                                <TextField
+                                    {...params}
+                                    onChange={(e) => handleChange(e.target.value)}
+                                    label="Search"
+                                    variant="outlined"
+                                    margin="dense"
+                                    color="secondary"
+                                    classes={{
+                                        root: classes.inputRoot
+                                    }}
+                                    InputProps={{
+                                        ...params.InputProps,
+                                        endAdornment: (
+                                            <React.Fragment>
+                                                {loading ? <CircularProgress color="secondary" size={20} /> : null}
+                                            </React.Fragment>
+                                        ),
+                                    }}
+                                />
+                            )}
+                            onChange={(event, value) =>
+                                history.push(`/character/${value.id}`)
+                            }
                         />
                     </div>
                 </Toolbar>
